c35b115b59d720cc7f7b1eb354ced91b5c7effdb
Victor Vieira <victorxdking@gmail.com>
2024-06-09 16:27:32 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Escreva sobre o que você tem interesse!</h3>
-      <p>💪MundoMAROMBA &copy; 2024</p>
+      <h3>Conteúdo honesto e de qualidade para te ajudar a alcançar seus objetivos</h3>
+      <p>💪MundoMAROMBA &copy; 2024. Todos os direitos reservados.</p>
     </footer>
   );
 };
